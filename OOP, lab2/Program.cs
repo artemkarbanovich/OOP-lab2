5fc216e0b17115374c04624c5a82cd660267b19a
Artem Karbanovich <artyomkarbanovich@gmail.com>
2020-09-14 16:10:26 +0300

    v3

--- OOP, lab2/Program.cs
+++ OOP, lab2/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
 namespace OOP__lab2
 {
@@ -7,8 +9,23 @@ namespace OOP__lab2
         static void Main(string[] args)
         {
             //Ex1();
-            Ex2();
+            //Ex2();
+            //Ex3();
+            //Ex4();
 
+            // Задание №5
+            Console.WriteLine("\n\n\t\tЗадание №5\n");
+
+            Console.WriteLine("Минимальный элемент: " + Ex5(new int[] { 4, -3, 10, -18, 22 }, "строка-путешественник").Item1);
+            Console.WriteLine("Максимальный элемент: " + Ex5(new int[] { 4, -3, 10, -18, 22 }, "строка-путешественник").Item2);
+            Console.WriteLine("Сумма всех элементов: " + Ex5(new int[] { 4, -3, 10, -18, 22 }, "строка-путешественник").Item3);
+            Console.WriteLine("Первый элемент в строке: " + Ex5(new int[] { 4, -3, 10, -18, 22 }, "строка-путешественник").Item4);
+
+            // Задание №6
+            Console.WriteLine("\n\n\t\tЗадание №6\n");
+
+            //Console.WriteLine(Ex6_1());
+            Console.WriteLine(Ex6_2());
         }
 
         static void Ex1()
@@ -111,24 +128,28 @@ namespace OOP__lab2
             int result = String.Compare(str1, str2);
             if(result < 0)
             {
-                Console.WriteLine("Строка str1 стоит перед str2\n");
+                Console.WriteLine("Строка str1 стоит перед str2");
             }
             else if(result > 0)
             {
-                Console.WriteLine("Строка str1 стоит после str2\n");
+                Console.WriteLine("Строка str1 стоит после str2");
             }
             else if(result == 0)
             {
-                Console.WriteLine("Строки равны\n");
+                Console.WriteLine("Строки равны");
             }
 
 
 
             // Задание №2b
+            Console.WriteLine("\n\n\t\tЗадание №2b\n");
+
+            int s0 = 10;
             string s1 = "строка 1 ";
             string s2 = "строка 2 ";
             string s3 = "строка 3 ";
             string s4 = "Вот такие вот дела";
+            
 
             Console.WriteLine(String.Concat(s1, s2) + "\n");
 
@@ -142,11 +163,225 @@ namespace OOP__lab2
 
             Console.WriteLine("\n\n" + s4.Substring(4, 9) + "\n");
 
+            Console.WriteLine(s4.Remove(0, 4) + "\n");
+
+
+            string strSource = "changed";
+            char[] destination = { 'T', 'h', 'e', ' ', 'i', 'n', 'i', 't', 'i', 'a', 'l', ' ',
+                'a', 'r', 'r', 'a', 'y' };
+            Console.WriteLine(destination);
+
+            strSource.CopyTo(0, destination, 4, strSource.Length);
+            Console.WriteLine(destination);
+
+
+            Console.WriteLine($"\n{ s0 } и { s1 }");
 
 
 
+            // Задание №2c
+            Console.WriteLine("\n\n\t\tЗадание №2c\n");
+
+            string freeStr = "";
+            string nullStr = null;
+            string dopStr = "qwer";
+
+            Console.Write(String.IsNullOrEmpty(freeStr) + "\n");
+            Console.Write(String.IsNullOrEmpty(nullStr) + "\n");
+
+            dopStr = dopStr.Remove(0, dopStr.Length);
+            Console.Write(String.IsNullOrEmpty(dopStr) + "\n");
 
 
+
+            // Задание №2d
+            Console.WriteLine("\n\n\t\tЗадание №2d\n");
+
+            StringBuilder strBuild = new StringBuilder("Привет мир");
+            strBuild.Append("!!");
+            strBuild.Insert(7, "компьютерный ");
+            strBuild.Remove(0, 7);
+            Console.WriteLine(strBuild);
         }
+
+        static void Ex3()
+        {
+            // Задание №3a
+            Console.WriteLine("\n\n\t\tЗадание №3a\n");
+
+            int[,] array1 = new int[4, 4];
+
+            Random rnd = new Random();
+            for (int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    array1[i, j] = rnd.Next(0, 10);
+                    Console.Write($"  { array1[i, j] }  ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+            // Задание №3b
+            Console.WriteLine("\n\n\t\tЗадание №3b\n");
+
+            string[] strArray = new string[] { "First", "Second", "Third" };
+
+            Console.WriteLine($"Длина массива: { strArray.Length }");
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                Console.WriteLine(strArray[i]);
+            }
+
+
+            Console.Write("\nКакой элемент заменить: ");
+            int elemChange = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\nНа что заменить ? ");
+            string strChange = Console.ReadLine();
+
+            strArray[elemChange - 1] = strChange;
+
+            Console.WriteLine("Массив после изменения:\n");
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                Console.WriteLine(strArray[i]);
+            }
+
+
+
+            // Задание №3с
+            Console.WriteLine("\n\n\t\tЗадание №3с\n");
+
+            int[][] steppedArray = new int[3][];
+
+            steppedArray[0] = new int[2];
+            steppedArray[1] = new int[3];
+            steppedArray[2] = new int[4];
+
+            Console.WriteLine("Введите первую строку:");
+            for (int i = 0; i < 2; i++)
+            {
+                steppedArray[0][i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите вторую строку:");
+            for (int i = 0; i < 3; i++)
+            {
+                steppedArray[1][i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите третью строку:");
+            for (int i = 0; i < 4; i++)
+            {
+                steppedArray[2][i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"  { steppedArray[0][i] }  ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"  { steppedArray[1][i] }  ");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++)
+            {
+                Console.Write($"  { steppedArray[2][i] }  ");
+            }
+
+
+
+            // Задание №3d
+            Console.WriteLine("\n\n\t\tЗадание №3d\n");
+
+            string[] array2 = new string[] { "строка 1", "строка 2", "string 3" };
+
+            var array2Var = array2;
+
+            for(int i = 0; i < array2Var.Length; i++)
+            {
+                Console.WriteLine(array2Var[i]);
+            }
+        }
+
+        static void Ex4()
+        {
+            // Задание №4a, b, c
+            Console.WriteLine("\n\n\t\tЗадание №4a\n");
+
+            (int a, string b, char c, string d, ulong e) = (18, "металл", '#', "лоджия", 19999);
+            var tuple1 = (19, true, "дом");
+
+            Console.WriteLine(tuple1.ToString());
+
+            Console.WriteLine($"\n{ a }");
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+
+
+
+            // Задание №4d
+            Console.WriteLine("\n\n\t\tЗадание №4d\n");
+            var firstTuple = Tuple.Create(9, 3);
+            var secondTuple = Tuple.Create(9, 3);
+            var check = EqualityComparer<Tuple<int, int>>.Default;
+
+            if (check.Equals(firstTuple, secondTuple))
+            {
+                Console.WriteLine("Первый кортеж равен второму");
+            }
+            else
+            {
+                Console.WriteLine("Первый кортеж не равен второму");
+            }
+        }
+
+        static Tuple<int, int, int, char> Ex5(int[] numbers, string str)
+        {
+            int minNum = 1000, maxNum = -1000, sumNum = 0;
+            
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                sumNum = sumNum + numbers[i];
+
+                if(numbers[i] < minNum)
+                {
+                    minNum = numbers[i];
+                }
+                if(numbers[i] > maxNum)
+                {
+                    maxNum = numbers[i];
+                }
+            }
+            char firstLetter = str[0];
+            
+            return new Tuple<int, int, int, char>(minNum, maxNum, sumNum, firstLetter);
+        }
+
+        static int Ex6_1()
+        {
+            int ten = 10;
+            checked
+            {
+                int intVar = 2147483647 + ten;
+                return intVar + ten;
+            }
+        }
+        static int Ex6_2()
+        {
+            int ten = 10;
+            unchecked
+            {
+                int intVar = 2147483647 + ten;
+                return intVar + ten;
+            }
+        }
+
     }
 }
